d2194f6e254952119235f19e6c3daaed0096934b
Dylan Vasquez <sistemas53@red5g.co>
2023-03-27 11:52:24 -0500

    añadiendo otro get

--- index.php
+++ index.php
@@ -6,14 +6,25 @@
 
         private $nombre;
 
+        private $email;
+
         public function getId () : string
         {
             return $this->id;
         }
 
+
+
         public function getNombre () : string
         {
             return $this->nombre;
+        }
+
+
+
+        public function getEmail () : string
+        {
+            return $this->email;
         }
     }
 ?>
